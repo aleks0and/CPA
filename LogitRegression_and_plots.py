b4eba5cad31f80e8ad2d9536310173ba3bda9d50
Sharlin Utke <sharlinutke@gmail.com>
2018-12-24 01:30:04 +0100

    Update for training and test dataset

--- LogitRegression_and_plots.py
+++ LogitRegression_and_plots.py
@@ -34,7 +34,7 @@ def descriptive_analysis_of_logit(logit_result, df, dv, ivs):
     skplt.metrics.plot_confusion_matrix(df[dv], logit_result.predict() > 0.5)
     plt.show()
 
-
+# this function needs to be updated with the test set
 def descriptive_analysis_of_logit_given_dataset(logit_result, data, df):
     print(logit_result.summary2())
     prediction = np.array([1-logit_result.predict(), logit_result.predict()])
@@ -54,56 +54,56 @@ def descriptive_analysis_of_logit_given_dataset(logit_result, data, df):
 
 
 
-# #Loading and preparing the data
-# df = pd.read_csv("TelcoCustomerChurn.csv")
-# df = data_preprocessing(df, standardize=True)
-#
-# # Filtering and fitting variables that should be included
-# logit_dv = 'Churn_Yes'
-# # adding an intercept
-# df['intercept'] = 1.0
-# df["tenure2"] = df["tenure"] ** 2
-# df["MonthlyCharges2"] = df["MonthlyCharges"] ** 2
-# df["MonthlyCharges3"] = df["MonthlyCharges"] ** 3
-#
-# # selecting desired columns
-# logit_ivs = [
-#  'tenure',
-#  'tenure2',
-#  'MonthlyCharges',
-#  'SeniorCitizen_Yes',
-#  'InternetService_Fiber optic',
-#  'Contract_Month-to-month',
-#  'PaymentMethod_Electronic check',
-#  'intercept'
-#  ]
-# sns.heatmap(df[logit_ivs].corr())
-# plt.show()
-# x_train, x_test, y_train, y_test = train_test_split(df[logit_ivs], df[logit_dv], test_size=0.30, random_state=42)
-# =========================================ACCURACY OF THE MODEL===================================================
-# logRegress = LogisticRegression()
-# logRegress.fit(x_train, y_train)
-# accuracy = logRegress.score(x_train, y_train)
-# =================================================================================================================
-# print(accuracy)
-# logit = sm.Logit(y_train, x_train)
-# logit_result = logit.fit()
-#
-# print(logit_result.summary2())
-#
-# prediction = np.array([1-logit_result.predict(), logit_result.predict()])
-# print(df.describe())
+#Loading and preparing the data
+df = pd.read_csv("TelcoCustomerChurn.csv")
+df = data_preprocessing(df, standardize=True)
+ 
+# Filtering and fitting variables that should be included
+logit_dv = 'Churn_Yes'
+# adding an intercept
+df['intercept'] = 1.0
+df["tenure2"] = df["tenure"] ** 2
+df["MonthlyCharges2"] = df["MonthlyCharges"] ** 2
+df["MonthlyCharges3"] = df["MonthlyCharges"] ** 3
+
+ # selecting desired columns
+logit_ivs = [
+  'tenure',
+  'tenure2',
+  'MonthlyCharges',
+  'SeniorCitizen_Yes',
+  'InternetService_Fiber optic',
+  'Contract_Month-to-month',
+  'PaymentMethod_Electronic check',
+  'intercept'
+  ]
+sns.heatmap(df[logit_ivs].corr())
+plt.show()
+x_train, x_test, y_train, y_test = train_test_split(df[logit_ivs], df[logit_dv], test_size=0.30, random_state=42)
+#=========================================ACCURACY OF THE MODEL===================================================
+logRegress = LogisticRegression()
+logRegress.fit(x_train, y_train)
+accuracy = logRegress.score(x_train, y_train)
+#=================================================================================================================
+print(accuracy)
+logit = sm.Logit(y_train, x_train)
+logit_result = logit.fit()
+
+print(logit_result.summary2())
+ 
+prediction = np.array([1-logit_result.predict(x_test), logit_result.predict(x_test)])
+print(df.describe())
 #lift curve
-# skplt.metrics.plot_lift_curve(y_train, prediction.T)
-# plt.show()
-# skplt.metrics.plot_ks_statistic(y_train, prediction.T)
-# plt.show()
-# skplt.metrics.plot_cumulative_gain(y_train, prediction.T)
-# plt.show()
-# skplt.metrics.plot_roc_curve(y_train, prediction.T)
-# plt.show()
-# skplt.metrics.plot_confusion_matrix(y_train, logit_result.predict() > 0.5)
-# plt.show()
+skplt.metrics.plot_lift_curve(y_test, prediction.T)
+plt.show()
+skplt.metrics.plot_ks_statistic(y_test, prediction.T)
+plt.show()
+skplt.metrics.plot_cumulative_gain(y_test, prediction.T)
+plt.show()
+skplt.metrics.plot_roc_curve(y_test, prediction.T)
+plt.show()
+skplt.metrics.plot_confusion_matrix(y_test, logit_result.predict(x_test) > 0.5)
+plt.show()
 
 
 #logit_result = perform_logit(df, logit_dv, logit_ivs)
